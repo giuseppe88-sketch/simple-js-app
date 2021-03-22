350e57bf6912fb453eda0a721dcd2766b15ad4f3
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-03-22 12:32:36 +0100

    add data objects to the pokemon array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-let favoriteFood = "Pizza"; 
+let pokemonList = [];
 
-alert("hello world");
-
-document.write(favoriteFood);
+pokemonList = [
+{
+name:"bulbasaur",
+height:0.7,
+type:"grass"
+},
+{
+name:"Fearow",
+height:1.2,
+type:"flying"
+},
+{name:"Dragonair",
+height:4,
+type:"dragon"}
+];
