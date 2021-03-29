1ef38f1d593a1363b7f56926eb036f535c1f3b6c
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-03-29 18:07:41 +0200

    add pokemonRepository IIFE

--- js/script.js
+++ js/script.js
@@ -1,39 +1,66 @@
-let pokemonList = [];
 
-let pokemon = "";
+let pokemonRepository= (function(){
+    let pokemonList = [];
 
-
-pokemonList = [
-{
-name:"Bulbasaur",
-height:0.7,
-type:"grass"
-},
-{
-name:"Fearow",
-height:1.2,
-type:"flying"
-},
-{name:"Dragonair",
-height:4,
-type:"dragon"}
-];
-
-
-for(let i= 0; i < pokemonList.length ; i++){
+    let pokemon = "";
+    
+    
+    pokemonList = [
+    {
+    name:"Bulbasaur",
+    height:0.7,
+    type:"grass"
+    },
+    {
+    name:"Fearow",
+    height:1.2,
+    type:"flying"
+    },
+    {name:"Dragonair",
+    height:4,
+    type:"dragon"}
+    ];
   
-    if(pokemonList[i].height > 1){
-        document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height})-Wow!! is bigger the one meter! <br>`)
-    }
-    else { document.write(`${pokemonList[i].name}  (height:${pokemonList[i].height}) <br>`)
+    function add(item){
+        if(typeof item === "object"){
+        pokemonList.push(item)
+       }
+       else alert("not possible")
+    };
+
+    function getAll(){
+        return pokemonList
     }
 
 
+    return {
+        add: add,
+        getAll: getAll
+    }
+
+
+})();
+
+
+
+function loopFunction(user){
+    if(user.height > 1){
+        document.write(`<h1>${user.name}</h1> <p><strong> height:</strong> ${user.height} WOW! Is Bigger then one Meter!</p><br>`)
+    }
+    else{
+        document.write(`<h1>${user.name}</h1> <p><strong> height:</strong> ${user.height} Meter</p> <br>`)
+    }
+}
+
+function findDragon(user){
+    if(user.type === "dragon"){
+        return user
+    }
 }
 
 
+pokemonRepository.getAll().forEach(loopFunction);
 
+let dragons=pokemonRepository.getAll().filter(findDragon);
 
-
-
-
+console.log(dragons)
